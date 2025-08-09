0f5c52bc8c64809df9ea041c610335a37d0c5f8b
Nelly <nelsonfang117@gmail.com>
2025-08-08 20:13:13 -0700

    fix for github pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/PersonalWebsite">
+      <App />
+    </BrowserRouter>
+
   </StrictMode>,
 )
